7a93b1d409d055c6ce165b5ccc89259ea4e3173a
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-11-29 16:33:46 +0100

    Init commit

---
+++ src/main/java/com/gambit/gameservice/GameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gambit.gameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameServiceApplication.class, args);
+    }
+
+}
